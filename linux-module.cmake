5fa002d2113de3a98e11470d7f4d482883500c45
gunnerDgd <ifstudiomail@naver.com>
2024-07-25 13:53:55 +0900

    [Change] Root Build Package Name Changed (popcorn -> module)

--- linux-module.cmake
+++ linux-module.cmake
@@ -1,7 +1,7 @@
 lib_from_repo(linux-core popcorn-dev main)
 dep          (linux-core)
 
-add_kernel_package(popcorn)
+add_kernel_package(module)
 add_kernel_module (popcorn)
 
 kernel_module_source (
@@ -15,9 +15,9 @@ kernel_module_source (
 kernel_module_include(popcorn ${CMAKE_SOURCE_DIR}/lib/linux-core/include)
 kernel_module_include(popcorn ${CMAKE_SOURCE_DIR}/lib/popcorn/include)
 
-kernel_package_include(popcorn ${CMAKE_SOURCE_DIR}/lib/linux-core/include)
-kernel_package_include(popcorn ${CMAKE_SOURCE_DIR}/lib/popcorn/include)
+kernel_package_include(module ${CMAKE_SOURCE_DIR}/lib/linux-core/include)
+kernel_package_include(module ${CMAKE_SOURCE_DIR}/lib/popcorn/include)
 
-kernel_package_build_host(popcorn)
+kernel_package_build_host(module)
 
-kernel_package_module(popcorn popcorn)
+kernel_package_module(module popcorn)
